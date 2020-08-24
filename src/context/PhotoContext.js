411caf7140b3ee77b844dda2d13a4b1ab5abf589
Benjamin Simon <b3nshi@gmail.com>
2020-08-24 20:06:38 +0200

    Performance - Implement response cache

--- src/context/PhotoContext.js
+++ src/context/PhotoContext.js
@@ -3,24 +3,34 @@ import axios from "axios";
 import { apiKey } from "../api/config";
 export const PhotoContext = createContext();
 
-const PhotoContextProvider = props => {
+const PhotoContextProvider = (props) => {
+  const [cache, setCache] = useState({});
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(true);
-  const runSearch = query => {
-    axios
-      .get(
-        `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
-      )
-      .then(response => {
-        setImages(response.data.photos.photo);
-        setLoading(false);
-      })
-      .catch(error => {
-        console.log(
-          "Encountered an error with fetching and parsing data",
-          error
-        );
-      });
+  const runSearch = (query) => {
+    if (cache[query]) {
+      setImages(cache[query]);
+      setLoading(false);
+    } else {
+      axios
+        .get(
+          `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
+        )
+        .then((response) => {
+          setCache({
+            ...cache,
+            [query]: response.data.photos.photo,
+          });
+          setImages(response.data.photos.photo);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log(
+            "Encountered an error with fetching and parsing data",
+            error
+          );
+        });
+    }
   };
   return (
     <PhotoContext.Provider value={{ images, loading, runSearch }}>
